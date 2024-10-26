d671a1dd78006c51569861b1e30d17c35e681d81
Multi538 <59820274+Multi538@users.noreply.github.com>
2024-10-26 15:33:20 +0200

    AI now generates LoRAs correctly

--- prompt.py
+++ prompt.py
@@ -1,62 +1,82 @@
-import os
+# revised script thanks to Brem-AI on reddit 🖤
+
 import torch
-from diffusers import FluxPipeline
+from diffusers import FlowMatchEulerDiscreteScheduler, AutoencoderKL
+from diffusers.models.transformers.transformer_flux import FluxTransformer2DModel
+from diffusers.pipelines.flux.pipeline_flux import FluxPipeline
+from transformers import CLIPTextModel, CLIPTokenizer,T5EncoderModel, T5TokenizerFast
 from huggingface_hub import login
-from accelerate import Accelerator
-from random import randint
-from time import time
+import time
 
-# Set the environment variable for PyTorch memory management
-os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
+# Login into Hugginface
+login("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") 
 
-# Log in to Hugging Face
-# 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
-login("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")
+# Model data type
+dtype = torch.bfloat16
 
-# Define your prompt
-prompt = """A image of a young woman with a dark goth aesthetic.
-She wears a black top, black ripped fishnet tights, a black mini-skirt, black leather shoes with thick soles.
-Her makeup is bold, with dark lipstick and heavy eyeliner. The angle is a wide shot."""
-# Load the model and move it to GPU
-torch.cuda.empty_cache() # empty the cache
-pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
+##############
+# LOAD MODEL #
+##############
+
+# Local Folders
+bfl_dir = "black-forest-labs/FLUX.1-dev"
+
+# Load model components
+scheduler       = FlowMatchEulerDiscreteScheduler.from_pretrained(bfl_dir, subfolder="scheduler", torch_dtype=dtype)
+text_encoder    = CLIPTextModel.from_pretrained(bfl_dir, subfolder="text_encoder", torch_dtype=dtype)
+tokenizer       = CLIPTokenizer.from_pretrained(bfl_dir, subfolder="tokenizer", torch_dtype=dtype, clean_up_tokenization_spaces=True)
+text_encoder_2  = T5EncoderModel.from_pretrained(bfl_dir, subfolder="text_encoder_2", torch_dtype=dtype)
+tokenizer_2     = T5TokenizerFast.from_pretrained(bfl_dir, subfolder="tokenizer_2", torch_dtype=dtype, clean_up_tokenization_spaces=True)
+vae             = AutoencoderKL.from_pretrained(bfl_dir, subfolder="vae", torch_dtype=dtype)
+
+# Load model
+transformer = FluxTransformer2DModel.from_pretrained(bfl_dir, subfolder="transformer", torch_dtype=dtype)
+
+############
+# PIPELINE #
+############
+
+# Create pipe
+pipe = FluxPipeline(
+  scheduler=scheduler,
+  text_encoder=text_encoder,
+  tokenizer=tokenizer,
+  text_encoder_2=text_encoder_2,
+  tokenizer_2=tokenizer_2,
+  vae=vae,
+  transformer=transformer
+)
 pipe.enable_sequential_cpu_offload()
 pipe.vae.enable_slicing()
 pipe.vae.enable_tiling()
 
-# Load LoRa weights before setting up with Accelerator
-pipe.load_lora_weights("Mutli/gothgirlflux", weight_name="ravengriim13.safetensors", adapter_name="ravengriim")
-pipe.load_lora_weights("Mutli/gothgirlflux", weight_name="swaggy16.safetensors", adapter_name="swaggy")
-pipe.set_adapters(["ravengriim", "swaggy"], adapter_weights=[1, 1])
+# Load LoRA
+pipe.load_lora_weights("D:/pinokio/api/stable-diffusion-webui-forge.git/app/models/Lora/bexicutes21.safetensors", adapter_name="bexicutes21")
+pipe.set_adapters("bexicutes21", adapter_weights=0.75)
 
-# Prepare pipeline with Accelerator (sequential offload handles device management)
-accelerator = Accelerator()
-pipe = accelerator.prepare(pipe)
+#############
+# INFERENCE #
+#############
 
-# Generate an image
-num_images = 1
-output_dir = "generated_images"
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+# Settings
+prompt    = "a women with fat cleavage and fat waist, black and white hair standing at a beach with a black bikini. Her body is turned to the side and she is facing off into the distance. the angel is wide. Tattoo that reads 'Mommy'."
+width     = 1080
+height    = 1920
+guidance  = 0
+steps     = 20
+seed      = 2011152402
 
-# Generate an image
-for i in range(num_images):
-    start_time = time()
-    output = pipe(
-        prompt, # The text that describes the image you want
-        height=1152,
-        width=896, # These define the size (in pixels) of the generated image
-        guidance_scale=7.5, # This controls how much the model should adhere to the prompt. A higher value (usually between 7.5 and 15) will make the model more faithful to the prompt, while a lower value allows for more creativity
-        num_inference_steps=20, # This defines how many steps the model takes during the image generation process. More steps typically result in better quality, but it takes longer
-        max_sequence_length=512, # This sets the maximum sequence length for the model’s input prompt
-        generator=torch.Generator("cpu").manual_seed(0)
-    )
-    end_time = time()
-    image = output.images[0]  # Access the first image from the output
-
-    # Save the image with a numbered filename
-    time_for_image = end_time - start_time
-    image_path = os.path.join(output_dir, f"image_{i+1}.png")
-    image.save(image_path)
-    print(f"Image took {time_for_image}")
-    print(f"Image {i+1} generated and saved as {image_path}")
+# Generation
+start_time = time.time()
+image = pipe(
+  prompt=prompt,
+  width=width,
+  height=height,
+  guidance_scale=guidance,
+  num_inference_steps=steps,
+  generator=torch.Generator("cpu").manual_seed(seed)
+).images[0]
+end_time = time.time()
+generation_time = end_time - start_time
+print(f"Generating image took: {generation_time} seconds")
+image.save("image.png")
