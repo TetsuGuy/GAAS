02843f3ea9589786dea3985e28f5f112cb56486c
Multi538 <59820274+Multi538@users.noreply.github.com>
2024-10-31 16:43:36 +0100

    remove style selection from randomize_prompt function

--- src/python/promptGen.py
+++ src/python/promptGen.py
@@ -70,7 +70,6 @@ def randomize_prompt():
     angle = random.choice(camera_angles)
     hair = random.choice(hairs)
     pose_choice = random.choice(pose)
-    style = random.choice(styles)
     
     new_prompt = f"photo of a woman {pose_choice} {location}. She is wearing {clothing}. Her makeup is {makeup_choice}. She has {hair}. The angle is a {angle}."
     
